a0e85cdd55a95b1f0634458cd32c76d02adfb8c1
Nauteirius <kamil.sordyl@student.uj.edu.pl>
2024-03-16 19:16:10 +0100

    new format of data

--- get_encoded_data.py
+++ get_encoded_data.py
@@ -6,10 +6,14 @@ if __name__ == '__main__':
     dataset = torch.load("modelstealing/data/ExampleModelStealingPub.pt")
     print(dataset.ids, dataset.imgs, dataset.labels)
 
-    generated = []
+    generated = {}
     for idx, (img_idx, img) in enumerate(zip(dataset.ids, dataset.imgs)):
         encoding = model_stealing(img)
-        generated.append((idx, img_idx, encoding))
+        #generated.append((idx, img_idx, encoding))
+        
+        generated['obrazki'].append(img_idx)
+        generated['reprezentacje'].append(encoding)
+        generated['id'].append(idx)
 
     with open('generated_data') as f:
         pickle.dump(generated, f)
